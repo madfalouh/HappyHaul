b3f49fe5ffc0f9e1c8df0a3c79c95e053d1c1b44
Mohamed Reda Falhi <70645288+madfalouh@users.noreply.github.com>
2023-03-16 20:43:05 -0500

    add deals section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
+import Banner from './component/banner/Banner'
+import FeauturedProducts from './component/featuredProducts/FeauturedProducts'
+import Features from './component/features/Features'
+import LastBanner from './component/lastBanner/LastBanner'
+import Nav from './component/nav/Nav'
+import NewArrivals from './component/NewArrivals/NewArrivals'
+import Sm_banner from './component/sm-banner/Sm_banner'
+import WelcomSection from './component/welcome-section/WelcomSection'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Nav></Nav>
+      <WelcomSection></WelcomSection>
+      <Features></Features>
+      <FeauturedProducts></FeauturedProducts>
+      <Banner></Banner>
+      <NewArrivals></NewArrivals>
+      <Sm_banner></Sm_banner>
+      <LastBanner></LastBanner>
     </div>
   )
 }
